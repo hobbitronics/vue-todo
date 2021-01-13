a4af64af7f222eb3b4b0e790d69aa0dfcb6537eb
hobbitronics <miguel.dw@gmail.com>
2021-01-13 10:08:48 -0600

    slideShow added

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,31 +1,31 @@
 <template>
-    <header class="header">
-        <span v-html="appName"></span>
-      <div id="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>
-      </div>
-    </header>
+  <header class="header">
+    <span v-html="appName"></span>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/slideShow">SlideShow</router-link>
+    </div>
+  </header>
 </template>
 
 <script>
 export default {
-    name: "Header",
-    props: ["appName"]
+  name: 'Header',
+  props: ['appName']
 }
 </script>
 
 <style scoped>
-  .header {
-    background: #333;
-    color: #fff;
-    text-align: center;
-    padding: 10px;
-  }
-  .header a {
-    color: #fff;
-    padding-right: 5px;
-    text-decoration: none;
-  }
-
+.header {
+  background: #333;
+  color: #fff;
+  text-align: center;
+  padding: 10px;
+}
+.header a {
+  color: #fff;
+  padding-right: 5px;
+  text-decoration: none;
+}
 </style>
