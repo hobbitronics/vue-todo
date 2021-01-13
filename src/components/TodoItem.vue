fee08382b0fdef23a82c819b0bf912bddfa0b53c
hobbitronics <miguel.dw@gmail.com>
2021-01-13 16:38:30 -0600

    vuefity, added completed, emit complete

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,49 +1,41 @@
 <template>
-    <div class="todo-item" >
-        <p>
-            <input v-model="todo.completed" :id=todo.id type="checkbox" >
-            <span :class="{'is-complete':todo.completed}">{{todo.title}}</span>
-            <button @click="$emit('del-todo', todo.id)" class="del">x</button>
-        </p>
-    </div>
+  <v-container>
+    <v-row justify="space-between">
+      <v-checkbox
+        v-model="completed"
+        :id="toString(todo.id)"
+        type="checkbox"
+        @click="$emit('complete', todo.id)"
+      />
+      <span :class="{ 'is-complete': todo.completed }">{{ todo.title }}</span>
+      <v-btn
+        icon
+        elevation="2"
+        @click="$emit('del-todo', todo.id)"
+        class="red float-right"
+        >x</v-btn
+      >
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-    name: "TodoItem",
-    props: ["todo"]
-//     methods: {
-//         markComplete () {
-//             this.todo.completed = !this.todo.completed
-//         }
-//     },
-//     mounted () {  //fires when mounted to dom
-//         const box = document.getElementById(this.todo.id)
-//         box.checked = this.todo.completed
-//         console.log(box)
-//     }
+  name: 'TodoItem',
+  props: ['todo'],
+  data() {
+    return {
+      completed: this.todo.completed
+    }
+  }
 }
 </script>
 
 <style scoped>
-    .todo-item {
-    background: #f4f4f4;
-    padding: 10px;
-    border-bottom: 1px #ccc dotted;
-  }
-  .is-complete {
-    text-decoration: line-through;
-  }
-  span {
-      padding-left: 1rem;
-  }
-  .del {
-    background: #ff0000;
-    color: #fff;
-    border: none;
-    padding: 5px 9px;
-    border-radius: 50%;
-    cursor: pointer;
-    float: right;
-  }
+.is-complete {
+  text-decoration: line-through;
+}
+span {
+  padding-left: 1rem;
+}
 </style>
