00941d2609ad3cbc7e894c9f2eb1c6dc58c9bc6a
hobbitronics <miguel.dw@gmail.com>
2021-01-13 16:38:51 -0600

    emit complete

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,23 +1,26 @@
 <template>
-    <div>
-        <div :key="todo.id" v-for="todo in todos">
-            <TodoItem :todo="todo" @del-todo="$emit('del-todo', todo.id)" />
-        </div>
+  <div>
+    <div :key="todo.id" v-for="todo in todos">
+      <TodoItem
+        :todo="todo"
+        @del-todo="$emit('del-todo', todo.id)"
+        @complete="$emit('complete', todo.id)"
+      />
     </div>
+  </div>
 </template>
 
 <script>
 import TodoItem from './TodoItem.vue'
 
 export default {
-    name: "Todos",
-    components: {
-        TodoItem
-    },
-    props: ["todos"]
+  name: 'Todos',
+  components: {
+    TodoItem
+  },
+  props: ['todos'],
+  data() {
+    return {}
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
